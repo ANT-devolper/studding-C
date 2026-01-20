98813a399b3806762cc4f44610af489e431b6afa
Antonio Guerreiro Borges <84242695+ANT-devolper@users.noreply.github.com>
2026-01-19 21:59:09 -0300

    feat: implemetation of the arena allocator fuctions

--- arena-allocator/main.c
+++ arena-allocator/main.c
@@ -4,6 +4,9 @@
 #include <string.h>
 #include <stdbool.h>
 
+/**
+ * Definition of the types will use on arena_allocator
+ */
 typedef int8_t i8;
 typedef int16_t i16;
 typedef int32_t i32;
@@ -16,23 +19,81 @@ typedef uint64_t u64;
 typedef i8 b8;
 typedef i32 b32;
 
+/**
+ * Convert human values on bytes
+ */
 #define KiB(n) ((u64)(n) << 10)
 #define MiB(n) ((u64)(n) << 20)
 #define GiB(n) ((u64)(n) << 30)
 
+/**
+ * Comparation min and max macros
+ */
 #define MIN(a, b) (((a) < (b)) ? (a) : (b))
 #define MAX(a,b) (((a) > (b)) ? (a) : (b))
+
+/**
+ * Aling macros
+ */
 #define ALING_UP_POU2(n, p) (((u64)(n) + ((u64)(p) - 1 )) & (~((u64)(p) - 1)))
+#define ARENA_BASE_POS (sizeof(mem_arena))
+#define ARENA_ALING (sizeof(void*))
 
 typedef struct {
     u64 capacity;
     u64 pos;
 } mem_arena;
 
-mem_arena* arena_create(u64 capity);
-void arena_destroy(mem_arena* arena);
+mem_arena* arena_create(u64 capacity);
 
-void* arena_push(mem_arena* arena, u64 size);
+void arena_destroy(mem_arena* arena);
+void* arena_push(mem_arena* arena, u64 size, b32 non_zero);
 void arena_pop(mem_arena* arena, u64 size);
 void arena_pop_to(mem_arena* arena, u64 pos);
 void arena_clear(mem_arena* arena);
+
+#define PUSH_STRUCT(arena, T) (T*)arena_push((arena), sizeof(T), false)
+#define PUSH_STRUCT_NZ(arena, T) (T*)arena_push((arena), sizeof(T), true)
+#define PUSH_ARRAY(arena, T, n) (T*)arena_push((arena), sizeof(T) * (n), false)
+#define PUSH_ARRAY_NZ(arena, T) (T*)arena_push((arena), sizeof(T) * (n), true)
+
+int main(void){
+
+    return 0;
+}
+
+
+mem_arena* arena_create(u64 capacity){
+    mem_arena* arena = (mem_arena*)malloc(capacity);
+    arena->capacity = capacity;
+    arena->pos = ARENA_BASE_POS;
+    return arena;
+}
+void arena_destroy(mem_arena* arena){
+    free(arena);
+}
+void* arena_push(mem_arena* arena, u64 size, b32 non_zero){
+    u64 pos_aligned = ALING_UP_POW2(arena->pos, ARENA_ALING);
+    u64 new_pos = pos_aligned + size;
+
+    if (new_pos > arena->capacity) return NULL;
+    
+    arena->pos = new_pos;
+
+    u8* out = (u8*)arena + pos_aligned;
+
+    if (!non_zero) memset (out, 0, size);
+
+    return out;
+}
+void arena_pop(mem_arena* arena, u64 size){
+    size = MIN(size, arena->pos - ARENA_BASE_POS);
+    arena->pos -= size;
+}
+void arena_pop_to(mem_arena* arena, u64 pos){
+    u64 size = pos <arena->pos ? arena->pos - pos : 0;
+    arena_pop(arena,size);
+}
+void arena_clear(mem_arena* arena){
+    arena_pop_to(arena, ARENA_BASE_POS);
+}
